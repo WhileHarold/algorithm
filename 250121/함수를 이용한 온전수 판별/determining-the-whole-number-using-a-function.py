26c2ada7e657a92d78fbd054e1bd51711490d2cd
WhileHarold <152210770+WhileHarold@users.noreply.github.com>
2025-01-21 16:43:40 +0900

    Title: 함수를 이용한 온전수 판별, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250121/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.py
+++ 250121/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.py
@@ -1,12 +1,21 @@
-a, b = map(int, input().split())
-
-# Write your code here!
-def onjeonsu(a, b):
-    cnt = 0
-    for i in range(a, b+1):
-        if i % 2 == 0 or i % 5 == 0 or (i % 3 == 0 and i % 9 != 0):
-            cnt += 1
-    print((b-a+1)-cnt)
+a, b = tuple(map(int, input().split()))
 
 
-onjeonsu(a,b)
+def judge_number(n):
+    if n % 2 == 0:
+        return False
+    elif n % 10 == 5:
+        return False
+    elif n % 3 == 0 and n % 9 != 0:
+        return False
+    else:
+        return True
+
+
+cnt = 0
+
+for i in range(a, b + 1):
+    if judge_number(i):
+        cnt += 1
+
+print(cnt)
