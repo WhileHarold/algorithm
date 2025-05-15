ba6cce1e3045105489d645dc8c0654b5f5407885
WhileHarold <152210770+WhileHarold@users.noreply.github.com>
2025-05-15 17:59:05 +0900

    Title: 키를 기준으로 정렬, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250515/키를 기준으로 정렬/sort-by-height.py
@@ -0,0 +1,19 @@
+class Student:
+    def __init__(self, name, height, weight):
+        self.name = name
+        self.height = height
+        self.weight = weight
+
+
+n = int(input())
+students = []
+
+for _ in range(n):
+    name, height, weight = input().split()
+    students.append(Student(name, int(height), int(weight)))
+
+
+students.sort(key=lambda x: x.height)
+
+for Student in students:
+    print(Student.name, Student.height, Student.weight)
