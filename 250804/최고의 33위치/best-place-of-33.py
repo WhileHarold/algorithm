1ef8f94e311451fdb0642c15f7bcfe272b3d8497
WhileHarold <152210770+WhileHarold@users.noreply.github.com>
2025-08-04 16:48:57 +0900

    Title: 최고의 33위치, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250804/최고의 33위치/best-place-of-33.py
+++ 250804/최고의 33위치/best-place-of-33.py
@@ -1,9 +1,9 @@
 n = int(input())
-grid = [list(map(int, input().split())) for _ in range(N)]
+grid = [list(map(int, input().split())) for _ in range(n)]
 
 max_coin = 0
 
-for i in range(n- 2):
+for i in range(n - 2):
     for j in range(n - 2):
         coin_count = 0
         for dx in range(3):
