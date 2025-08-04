ac0e5745f820336faf138e82382757fd64b98180
WhileHarold <152210770+WhileHarold@users.noreply.github.com>
2025-08-04 16:48:23 +0900

    Title: 최고의 33위치, Time: 75ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 250804/최고의 33위치/best-place-of-33.py
+++ 250804/최고의 33위치/best-place-of-33.py
@@ -1,14 +1,14 @@
 n = int(input())
-grid = [list(map(int, input().split())) for _ in range(n)]
+grid = [list(map(int, input().split())) for _ in range(N)]
 
 max_coin = 0
 
-for i in range(n - 2):
+for i in range(n- 2):
     for j in range(n - 2):
-        count_coin = 0
+        coin_count = 0
         for dx in range(3):
             for dy in range(3):
-                count_coin += grid[i + dx][i + dy]
-                max_coin = max(count_coin, max_coin)
+                coin_count += grid[i + dx][j + dy]
+        max_coin = max(max_coin, coin_count)
 
 print(max_coin)
